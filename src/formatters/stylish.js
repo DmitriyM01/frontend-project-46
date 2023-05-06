449668015d8fb339554782970b5cf8357260250e
DmitriyM <mironowd697@gmail.com>
2023-05-06 16:11:22 +0600

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,9 +9,7 @@ const stringify = (node, depth = 1) => {
   const currentIndent = indent.repeat(depth);
   const indentForBracket = indent.repeat(depth - 1);
 
-  const lines = Object
-  .entries(node)
-  .map(([key, val]) => `${currentIndent}${key}: ${stringify(val, depth + 1)}`);
+  const lines = Object.entries(node).map(([key, val]) => `${currentIndent}${key}: ${stringify(val, depth + 1)}`);
 
   return [
     '{',
@@ -24,22 +22,22 @@ const makeStylish = (tree) => {
   const iter = (node, depth = 1) => {
     const indent = '    ';
     const currentIndent = indent.repeat(depth - 1);
-      switch (node.status) {
-        case 'root':
-          return `{${node.children.map((child) => iter(child)).join('')}\n}`;
-        case 'nested':
-          return `\n${currentIndent}    ${node.key}: {${node.children.map((child) => iter(child, depth + 1)).join('')}\n    ${currentIndent}}`;
-        case 'changed':
-          return `\n${currentIndent}  - ${node.key}: ${stringify(node.value[0], depth + 1)}\n${currentIndent}  + ${node.key}: ${stringify(node.value[1], depth + 1)}`;
-        case 'unchanged':
-          return `\n${currentIndent}    ${node.key}: ${stringify(node.value, depth + 1)}`;
-        case 'added':
-          return `\n${currentIndent}  + ${node.key}: ${stringify(node.value, depth + 1)}`;
-        default:
-          return `\n${currentIndent}  - ${node.key}: ${stringify(node.value, depth + 1)}`;
-      }
+    switch (node.status) {
+      case 'root':
+        return `{${node.children.map((child) => iter(child)).join('')}\n}`;
+      case 'nested':
+        return `\n${currentIndent}    ${node.key}: {${node.children.map((child) => iter(child, depth + 1)).join('')}\n    ${currentIndent}}`;
+      case 'changed':
+        return `\n${currentIndent}  - ${node.key}: ${stringify(node.value[0], depth + 1)}\n${currentIndent}  + ${node.key}: ${stringify(node.value[1], depth + 1)}`;
+      case 'unchanged':
+        return `\n${currentIndent}    ${node.key}: ${stringify(node.value, depth + 1)}`;
+      case 'added':
+        return `\n${currentIndent}  + ${node.key}: ${stringify(node.value, depth + 1)}`;
+      default:
+        return `\n${currentIndent}  - ${node.key}: ${stringify(node.value, depth + 1)}`;
+    }
   };
-    return iter(tree);
+  return iter(tree);
 };
 
 export default makeStylish;

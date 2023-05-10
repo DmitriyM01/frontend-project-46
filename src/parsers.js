168ffda4dbcca9c5b845daaab59ff4d88d9806a6
DmitriyM <mironowd697@gmail.com>
2023-05-10 14:50:00 +0600

    trying find errors

--- src/parsers.js
+++ src/parsers.js
@@ -10,7 +10,7 @@ const getName = (filename) => path.basename(filename);
 const parser = (file) => {
     const name = getName(file);
     const extension = getExtension(name);
-    const p = process.cwd();
+    const p = `${process.cwd()}`;
     // path.resolve(process.cwd(), (`__fixtures__/${file}`));
     return extension !== '.json' ? yaml.load(fs.readFileSync(p, 'utf-8')) : JSON.parse(fs.readFileSync(p, 'utf-8'));
 };

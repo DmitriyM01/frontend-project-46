1b8f250a0aa1ba88a99d3d76b9f36ef2f9a3345c
DmitriyM <mironowd697@gmail.com>
2023-05-11 13:42:48 +0600

    qfix

--- src/parsers.js
+++ src/parsers.js
@@ -10,11 +10,10 @@ const getName = (filename) => path.basename(filename);
 const parser = (file) => {
     const name = getName(file);
     const extension = getExtension(name);
-    console.log(extension);
+    console.log(`Расширение - ${extension}`);
     const normalizePath = path.resolve(process.cwd(), (`__fixtures__/${name}`));
+    console.log(`Путь - ${normalizePath}`);
     return extension !== '.json' ? yaml.load(fs.readFileSync(normalizePath, 'utf-8')) : JSON.parse(fs.readFileSync(normalizePath, 'utf-8'));
 };
-
-console.log(parser('f1.json'));
 
 export default parser;
